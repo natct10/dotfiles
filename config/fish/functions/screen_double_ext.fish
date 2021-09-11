1c1fba2b2706ece197a358f883639a8bb0dd1fbf
natct10 <nathanael.cretin@substra.org>
2021-09-11 19:42:19 +0200

    Ajoute le double écran.

---
+++ config/fish/functions/screen_double_ext.fish
@@ -0,0 +1,3 @@
+function screen_double_ext --wraps='~/dev/Dotfiles/screenlayout/screen_double_ext.sh' --description 'alias screen_double_ext ~/dev/Dotfiles/screenlayout/screen_double_ext.sh'
+  ~/dev/Dotfiles/screenlayout/screen_double_ext.sh $argv; 
+end
