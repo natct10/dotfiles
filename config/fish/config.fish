58b4e5581094c1937260fdb6b5d60d8577993e4b
natct <ncretin@easter-eggs.com>
2024-02-13 20:37:59 +0100

    2024 Refresh

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,6 @@ set PATH -U ~/.npm-global/bin/ $PATH
 set PATH ~/.npm/bin/ $PATH
 set PATH ./node_modules/.bin $PATH
 set PATH /usr/local/go/bin $PATH
+set PATH /home/nat/.local/bin $PATH
 direnv hook fish | source
 
